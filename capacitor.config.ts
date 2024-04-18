a31d5f0afb9fea854468804fd9502927d6343af3
Victor Jesus Fernandez Noguer <victorjesusfernandeznoguer@gmail.com>
2024-04-18 13:56:12 +0200

    [FEAT] Initial & Final commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: '06-qrscanner',
+  appId: 'io.ionic.qrscanner.victor',
+  appName: 'QrScanner',
   webDir: 'www'
 };
 
