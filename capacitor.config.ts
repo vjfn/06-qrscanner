f9633f70e80a1f8d331b09886c50745aa62f3b20
Victor Jesus Fernandez Noguer <victorjesusfernandeznoguer@gmail.com>
2024-04-18 12:07:14 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '06-qrscanner',
+  webDir: 'www'
+};
+
+export default config;
